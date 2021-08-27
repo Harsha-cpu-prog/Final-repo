c34b2246e2d1cccd93f139b6a5504709ba7d5318
Harsha-cpu-prog <harshanenwani121@gmail.com>
2021-08-27 13:33:41 +0530

    adding files

---
+++ server/database/user/index.js
@@ -0,0 +1,16 @@
+import moongoose  from "moongoose";
+
+const UserSchema = new moongoose.Schema({
+    
+          fullname: { type: String, required: true },
+          email: { type: String, required: true },
+          password: { type: String },
+          address: [{ detail: { type: String }, for: { type: String } }],
+          phoneNumber: [{ type: Number }],
+},
+{
+    timestamps:true,
+}
+);
+
+export const UserModel = mongoose.model("Users", UserSchema);
