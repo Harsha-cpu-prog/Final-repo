5ca186df0e0e00d1c6d43a7e939dad1903904023
Harsha-cpu-prog <harshanenwani121@gmail.com>
2021-08-30 15:28:27 +0530

    new changes

--- server/index.js
+++ server/index.js
@@ -1,16 +1,39 @@
+// importing env variables
+require("dotenv").config();
+
+// libraries
 import express from"express";
 import cors from "cors";
 import helmet from "helmet";
 
-const zomato =express(
 
-// application middlewares
+// microservies routes
+import Auth from "./API/Auth";
+
+// databse connection
+import ConnectDB from"./database/connection";
+
+
+const zomato =express();
+
+// application routes
 zomato.use(express.json());
 zomato.use(express.urlencoded({ extended :false }));
 zomato.use(helmet());
 zomato.use(cors());
 
+// application route
+zomato.use ("/auth",Auth);
+
+
 zomato.get ("/", (req,res) => res.json ({message:"Setup sucess"}));
-zomato.listen (9000, ()=>  console.log ("Server is runnings"));
+zomato.listen (9000, ()=>
+ ConnectDB()
+ .then(() => console.log ("Server is running "))
+ .catch(() =>console.log ("Server is running but databse connection failed"))
+
+);
+
+
 
 
